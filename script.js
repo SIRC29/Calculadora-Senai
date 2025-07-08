9454c3a7dfa3d019681935446f4774b33a0492ec
SIRC29 <cristianomartins.x09@gmail.com>
2025-07-08 09:09:20 -0300

    Add files via upload
    Esta é uma calculadora simples e funcional que fiz.

---
+++ script.js
@@ -0,0 +1,70 @@
+const display = document.getElementById('display');
+let valorAtual = '0';
+valorAnterior = '';
+let operador = '';
+let novoNumero = false;
+
+function atualizarDisplay() {
+    display.textContent = valorAtual;
+}
+document.querySelectorAll('[data-num]').forEach((botao) => {
+    botao.addEventListener('click', () => {
+        if (novoNumero) {
+            valorAtual = botao.dataset.num;
+            novoNumero = false;
+        } else {
+            if (valorAtual === '0') {
+                valorAtual = botao.dataset.num;
+            } else {
+                valorAtual += botao.dataset.num;
+            }
+        }
+        atualizarDisplay();
+    });
+});
+
+document.querySelectorAll('[data-op]').forEach((botao) => {
+    botao.addEventListener('click', () => {
+        if (operador && !novoNumero) {
+            calcular();
+        }
+        operador = botao.dataset.op;
+        valorAnterior = valorAtual;
+        novoNumero = true;
+    });
+});
+
+document.getElementById('equal').addEventListener('click', () => {
+   valorAnterior = parseFloat(valorAnterior);
+   valorAtual = parseFloat(valorAtual);
+   operador = operador.trim();
+   if (operador === '+') {
+       valorAtual = valorAnterior + valorAtual;
+   }
+   else if (operador === '-') {
+       valorAtual = valorAnterior - valorAtual;
+   }
+   else if (operador === '*') {
+       valorAtual = valorAnterior * valorAtual;
+   }
+   else if (operador === '/') {
+       if (valorAtual === 0) {
+           alert('Divisão por zero não é permitida.');
+           return;
+       }
+       valorAtual = valorAnterior / valorAtual;
+   }
+   operador = '';
+   novoNumero = true;
+   atualizarDisplay();
+});
+
+document.getElementById('clear').addEventListener('click', () => {
+    valorAtual = '0';
+    valorAnterior = '';
+    operador = '';
+    novoNumero = false;
+    atualizarDisplay();
+}); 
+
+
